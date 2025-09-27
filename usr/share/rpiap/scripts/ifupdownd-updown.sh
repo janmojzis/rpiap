d21a23ce16eb3c3fb5b2b88b17a6c4c235209166
Jan Mojžíš <jan.mojzis@gmail.com>
2025-09-27 19:58:27 +0200

    ifupdownd-updown.sh run/terminate wpasupplicant

--- usr/share/rpiap/scripts/ifupdownd-updown.sh
+++ usr/share/rpiap/scripts/ifupdownd-updown.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
+set -e
 
-case "$2" in
-up)
-  /sbin/ifup $1
+script=$0
+interface=$1
+phase=$2
+
+case "${phase}" in
+
+  up)
+    echo "${script}: ${interface}: phase=${phase}, executing /sbin/ifup ${interface}" >&2
+    /sbin/ifup "${interface}"
+    if [ xinterface = xwlan1 ]; then
+      echo "${script}: ${interface}: phase=${phase}, running wpasupplicant on ${interface}" >&2
+      svc -tu /etc/service/rpiap_wpasupplicant
+    fi
   ;;
-down)
-  /sbin/ifdown $1
+
+  down)
+    echo "${script}: ${interface}: phase=${phase}, executing /sbin/ifdown ${interface}" >&2
+    /sbin/ifdown "${interface}"
+    if [ xinterface = xwlan1 ]; then
+      echo "${script}: ${interface}: phase=${phase}, stopping wpasupplicant on ${interface}" >&2
+      svc -d /etc/service/rpiap_wpasupplicant
+    fi
+  ;;
+
+  *)
+    echo "${script}: unknown phase=\"${phase}\"" >&2
+    exit 1
   ;;
 esac
+
+exit 0
