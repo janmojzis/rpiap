ac781b72f7f390ea05c53a2491cd0bd13722ef66
Jan Mojžíš <jan.mojzis@gmail.com>
2025-09-29 19:46:35 +0200

    var/lib/rpiap/service/wpasupplicant -> var/lib/rpiap/service/wpasupplicant_wlan1

--- usr/share/rpiap/scripts/ifupdownd-updown.sh
+++ usr/share/rpiap/scripts/ifupdownd-updown.sh
@@ -10,18 +10,18 @@ case "${phase}" in
   up)
     echo "${script}: ${interface}: phase=${phase}, executing /sbin/ifup ${interface}" >&2
     /sbin/ifup "${interface}"
-    if [ x"${interface}" = xwlan1 ]; then
+    if [ -h "/etc/service/rpiap_wpasupplicant_${interface}" ] && [ -f "/etc/rpiap/wpasupplicant/${interface}.conf" ]; then
       echo "${script}: ${interface}: phase=${phase}, running wpasupplicant on ${interface}" >&2
-      svc -tu /etc/service/rpiap_wpasupplicant
+      svc -tu "/etc/service/rpiap_wpasupplicant_${interface}"
     fi
   ;;
 
   down)
     echo "${script}: ${interface}: phase=${phase}, executing /sbin/ifdown ${interface}" >&2
     /sbin/ifdown "${interface}"
-    if [ x"${interface}" = xwlan1 ]; then
+    if [ -h "/etc/service/rpiap_wpasupplicant_${interface}" ] && [ -f "/etc/rpiap/wpasupplicant/${interface}.conf" ]; then
       echo "${script}: ${interface}: phase=${phase}, stopping wpasupplicant on ${interface}" >&2
-      svc -d /etc/service/rpiap_wpasupplicant
+      svc -d "/etc/service/rpiap_wpasupplicant_${interface}"
     fi
   ;;
 
