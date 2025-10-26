18af73d47205f17c436aafa31a2711ce7504f150
Jan Mojžíš <jan.mojzis@firma.seznam.cz>
2025-10-26 20:04:25 +0100

    ifupdownd.device.d/90-sysctl.sh add

---
+++ usr/share/rpiap/ifupdownd.device.d/90-sysctl.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+interface=$1
+phase=$2
+
+
+# XXX: backward compatibility
+if [ ! -f /var/lib/rpiap/env/lan ] && [ x"${interface}" = xwlan0 ]; then
+  # LAN
+  exit 0
+fi
+
+if grep "^${interface}"'$' /var/lib/rpiap/env/lan 2>/dev/null; then
+  # LAN
+  exit 0
+fi
+
+case "${phase}" in
+
+  up)
+    echo 2 > "/proc/sys/net/ipv6/conf/${interface}/accept_ra"
+    echo 1 > "/proc/sys/net/ipv6/conf/${interface}/autoconf"
+  ;;
+
+  down)
+    if [ -e "/proc/sys/net/ipv6/conf/${interface}/accept_ra" ]; then
+      echo 0 > "/proc/sys/net/ipv6/conf/${interface}/accept_ra"
+    fi
+    if [ -e "/proc/sys/net/ipv6/conf/${interface}/autoconf" ]; then
+      echo 0 > "/proc/sys/net/ipv6/conf/${interface}/autoconf"
+    fi
+  ;;
+
+  *)
+    exit 1
+  ;;
+esac
+
+exit 0
