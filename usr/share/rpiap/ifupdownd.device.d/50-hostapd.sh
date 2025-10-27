c068af0c2dd642d41d3d2c68861c9e67edbd33c9
Jan Mojžíš <jan.mojzis@firma.seznam.cz>
2025-10-27 22:25:22 +0100

    50-hostapd.sh add logging

--- usr/share/rpiap/ifupdownd.device.d/50-hostapd.sh
+++ usr/share/rpiap/ifupdownd.device.d/50-hostapd.sh
@@ -1,29 +1,44 @@
 #!/bin/sh
 
-interface=$1
+ifname=$1
 phase=$2
+basename="`basename $0`"
 
-# XXX - currently only wlan0 supported
-if [ x"${interface}" != xwlan0 ]; then
+if ! grep -Fxq -- "${ifname}" /var/lib/rpiap/env/lan; then
+  iftype=WAN
+else
+  iftype=LAN
+fi
+
+log() {
+  text=$1
+  echo "${basename}: INFO: ${iftype} ${ifname}: ${phase}: ${text}"
+}
+
+if [ x"${ifname}" != xwlan0 ]; then
+  # currently only wlan0 supported
   exit 0
 fi
 
-if ! grep -Fxq -- "${interface}" /var/lib/rpiap/env/lan; then
+if ! grep -Fxq -- "${ifname}" /var/lib/rpiap/env/lan; then
+  # ifname is not in LAN configuration
   exit 0
 fi
 
 case "${phase}" in
-
   up)
     svc -u /etc/service/rpiap_hostapd
+    log "running hostapd on ${ifname}"
   ;;
 
   down)
     svc -d /etc/service/rpiap_hostapd
+    log "stopping hostapd on ${ifname}"
   ;;
 
   *)
-    exit 1
+    # nothing to do
+    exit 0
   ;;
 esac
 
